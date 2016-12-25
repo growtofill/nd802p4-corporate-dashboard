9c8e77aad2af2728c68eb6f65adc662b5ab664ea
Pavlo Mykhalov <pmykhalov@gmail.com>
2016-12-25 15:27:22 +0200

    Add polling for issues

--- app/controllers/issues.js
+++ app/controllers/issues.js
@@ -1,22 +1,41 @@
 import Ember from 'ember';
 
-const headers = [
-  { title: 'Submission Timestamp', path: 'created.at' },
-  { title: 'Customer Name', path: 'user.login' },
-  { title: 'Customer Email Address', path: 'user.login' },
-  { title: 'Description', path: 'title' },
-  { title: 'Open/Closed Status', path: 'state' },
-  { title: 'Closed Timestamp', path: 'closed_at' },
-  { title: 'Employee Name', path: 'assignee.login' },
-];
+const pollInterval = 60000;
+const mapIssues = issues => issues.map(issue => ({
+  createdAt: issue.created_at,
+  userLogin: issue.user.login,
+  userEmail: `${issue.user.login}@users.noreply.github.com`,
+  title: issue.title,
+  state: issue.state,
+  closedAt: issue.closed_at,
+  assigneeLogin: issue.assignee ? issue.assignee.login : null
+}));
 
 export default Ember.Controller.extend({
-  headers,
+  issues: Ember.computed('model', function () {
+    const { issues } = this.get('model');
+    return mapIssues(issues);
+  }),
   issuesSortDef: ['created_at:desc'],
-  sortedIssues: Ember.computed.sort('model', 'issuesSortDef'),
+  sortedIssues: Ember.computed.sort('issues', 'issuesSortDef'),
   actions: {
     sortIssues(sortDef) {
       this.set('issuesSortDef', [sortDef]);
     }
+  },
+  pollUrl() {
+    const { url } = this.get('model');
+    const poll = () => {
+      Ember.$.getJSON(url)
+        .then(issues => {
+          this.set('model', { url, issues });
+          this.pollTimeout = setTimeout(poll, pollInterval);
+        });
+    };
+
+    this.pollTimeout = setTimeout(poll, pollInterval);
+  },
+  destroy() {
+    clearTimeout(this.pollTimeout);
   }
 });
--- app/routes/issues.js
+++ app/routes/issues.js
@@ -1,17 +1,14 @@
 import Ember from 'ember';
 
+const url = 'https://api.github.com/repos/facebook/react/issues';
+
 export default Ember.Route.extend({
   model() {
-    return Ember.$
-      .getJSON('https://api.github.com/repos/facebook/react/issues')
-      .then(issues => issues.map(issue => ({
-        createdAt: issue.created_at,
-        userLogin: issue.user.login,
-        userEmail: `${issue.user.login}@users.noreply.github.com`,
-        title: issue.title,
-        state: issue.state,
-        closedAt: issue.closed_at,
-        assigneeLogin: issue.assignee ? issue.assignee.login : null
-      })));
+    return Ember.$.getJSON(url)
+      .then(issues => ({ url, issues }));
+  },
+  setupController(controller, model) {
+    this._super(controller, model);
+    controller.pollUrl();
   }
 });
